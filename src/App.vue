f85b8a342f0f8309d0cc331d9b7557c98cffb8f8
geunheejung <geunhee0212@gmail.com>
2023-09-07 09:25:28 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -18,7 +18,6 @@
 }
 
 nav {
-  padding: 30px;
 }
 
 nav a {
